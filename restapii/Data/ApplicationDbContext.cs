4672aae927ea10962ba87f32dcf769e7a37dc6b5
Mertisk <116942476+Mertisk@users.noreply.github.com>
2024-08-10 13:46:09 +0300

    Add project files.

---
+++ restapii/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using restapii.Model;
+
+namespace api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+
+}
---
+++ restapii/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace restapii.Model
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
